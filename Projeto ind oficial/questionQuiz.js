b4b19bf7ab703edcc59404476fb7e5fc1c3478de
fortega007 <felipe.bonifacio@sptech.school>
2023-05-15 12:47:41 -0300

    Subindo Projeto individual

---
+++ Projeto ind oficial/questionQuiz.js
@@ -0,0 +1,11 @@
+
+  function processarQuiz() {
+    let pontuacao = 0;
+    
+    // Pergunta 1
+    if (document.querySelector('input[name="q1"]:checked').value === 'b') {
+      pontuacao += 1;
+    }
+    
+    // Pergunta 2
+    if (document.querySelector);
